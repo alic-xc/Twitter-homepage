866a5a3f4b903d35dda5ca5fe583c854b8dacb5a
alic-xc <aladelekan187@gmail.com>
2019-06-12 14:59:39 +0100

    update ui for Bird section

--- src/components/Bird.js
+++ src/components/Bird.js
@@ -1,25 +1,29 @@
 import React,{ Component } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {  faSearch, faUserFriends, faComment } from '@fortawesome/free-solid-svg-icons'
+
 
 export const Bird = () =>{
 
     return (
-        <div className="bird" style={birdStyle}>
-            <figure style={figureStyle} ><span></span>Follow your interest.</figure>
-            <figure style={figureStyle} ><span></span>Hear what people are talking about</figure>
-            <figure style={figureStyle} ><span></span>Join the conversation</figure>
+        <div className="bird">
+            <section>
+            <figure style={figureStyle} ><span><FontAwesomeIcon icon={faSearch} size="2x" /></span>Follow your interest.</figure>
+            <figure style={figureStyle} ><span><FontAwesomeIcon icon={faUserFriends} size="2x" /></span>Hear what people are talking about</figure>
+            <figure style={figureStyle} ><span><FontAwesomeIcon icon={faComment} size="2x" /></span>Join the conversation</figure>
+            </section>
         </div>
     )
 }
 
 
-const birdStyle = {
-    background:"#1da1f2"
-}
+
 const figureStyle = {
     fonSize:"18px",
     fontWeight:"bolder",
     fontFamily:"Segoe UI, Arial, sans-serif",
     lineHeight:"45px",
-    color:"white"
-    
+    color:"white",
+    alignSelf:"center",
 }
+
